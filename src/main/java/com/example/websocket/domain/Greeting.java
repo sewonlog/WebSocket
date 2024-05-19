9075a03ff9336724468c9a7c69e242fd9b89fabc
sewon <sewonlog@gmail.com>
2024-05-19 19:10:30 +0900

    first commit

---
+++ src/main/java/com/example/websocket/domain/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String name;
+}
---
+++ src/main/java/com/example/websocket/domain/HelloMessage.java
@@ -0,0 +1,12 @@
+package com.example.websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+    private String content;
+}
